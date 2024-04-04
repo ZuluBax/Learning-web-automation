bec6a9c4db8671d58afcddf30e4a58d0094a8a90
ZuluBax <bjw5891@gmail.com>
2024-04-04 20:34:37 +0100

    black formatting

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 
-def before_all(context):
-	options = webdriver.ChromeOptions()
-	options.add_argument("--headless=new")
 
-	context.browser = webdriver.Chrome(options=options)
+def before_all(context):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+
+    context.browser = webdriver.Chrome(options=options)
+
 
 def after_all(context):
-	context.browser.quit()
+    context.browser.quit()
--- features/steps/google.py
+++ features/steps/google.py
@@ -7,9 +7,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-@when('we visit google')
+@when("we visit google")
 def step(context):
-    context.browser.get('https://www.google.com')
+    context.browser.get("https://www.google.com")
 
 
 @then('it does have a title "Google"')
@@ -19,50 +19,62 @@ def step(context):
 
 @given("I am on the google home page")
 def step_impl(context):
-    context.execute_steps(u'''
+    context.execute_steps(
+        """
    when we visit google
    then it does have a title "Google"
-   ''')
+   """
+    )
     wait = WebDriverWait(context.browser, timeout=10)
-    wait.until(lambda d: context.browser.find_element(By.ID, 'L2AGLb')).is_displayed()
-    context.browser.find_element(By.ID, 'L2AGLb').click()
+    wait.until(lambda d: context.browser.find_element(By.ID, "L2AGLb")).is_displayed()
+    context.browser.find_element(By.ID, "L2AGLb").click()
 
 
 @when("I search for twitter")
 def step_impl(context):
     wait = WebDriverWait(context.browser, timeout=10)
-    wait.until(lambda d: context.browser.find_element(By.ID, 'APjFqb')).is_displayed()
-    context.browser.find_element(By.ID, 'APjFqb').send_keys('twitter')
-    context.browser.find_element(By.ID, 'APjFqb').send_keys(Keys.RETURN)
+    wait.until(lambda d: context.browser.find_element(By.ID, "APjFqb")).is_displayed()
+    context.browser.find_element(By.ID, "APjFqb").send_keys("twitter")
+    context.browser.find_element(By.ID, "APjFqb").send_keys(Keys.RETURN)
 
 
 @then("I see twitter in the search results")
 def step_impl(context):
     wait = WebDriverWait(context.browser, timeout=10)
-    wait.until(lambda d: context.browser.find_element(By.XPATH,
-                                                      '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a')).is_enabled()
-    result_name = context.browser.find_element(By.XPATH,
-                                               '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/h3').text
+    wait.until(
+        lambda d: context.browser.find_element(
+            By.XPATH,
+            '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a',
+        )
+    ).is_enabled()
+    result_name = context.browser.find_element(
+        By.XPATH,
+        '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/h3',
+    ).text
     assert result_name == "X. It's what's happening / X"
 
 
 @given("I have searched for Twitter on google")
 def step_impl(context):
-    context.execute_steps(u'''
+    context.execute_steps(
+        """
        given I am on the google home page
        when I search for twitter
        then I see twitter in the search results
-       ''')
+       """
+    )
 
 
 @when("I click on Twitter in the search results")
 def step_impl(context):
-    context.browser.find_element(By.XPATH,
-                                 '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a').click()
+    context.browser.find_element(
+        By.XPATH, '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a'
+    ).click()
+
 
 @then("I am on the Twitter home page")
 def step_impl(context):
     wait = WebDriverWait(context.browser, timeout=10)
     wait.until(lambda d: context.browser.title is not None)
-    title= context.browser.title
+    title = context.browser.title
     assert title == "X. It’s what’s happening / X", "actual: " + title
