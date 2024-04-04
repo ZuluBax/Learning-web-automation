8f4f1b05762f4d3a22ad860beb4a73a54e7c2b6f
ZuluBax <bjw5891@gmail.com>
2024-04-04 20:29:59 +0100

    added more tests getting to twitter home page

--- features/steps/google.py
+++ features/steps/google.py
@@ -1,6 +1,6 @@
 import time
 
-from behave import when, then
+from behave import when, then, given
 from selenium.common import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
@@ -9,21 +9,60 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 @when('we visit google')
 def step(context):
-   context.browser.get('https://www.google.com')
+    context.browser.get('https://www.google.com')
 
-@then('it should have a title "Google"')
+
+@then('it does have a title "Google"')
 def step(context):
-   assert context.browser.title == "Google"
-   wait = WebDriverWait(context.browser, timeout=10)
+    assert context.browser.title == "Google"
 
-   wait.until(lambda d: context.browser.find_element(By.ID, 'L2AGLb')).is_displayed()
-   context.browser.find_element(By.ID, 'L2AGLb').click()
 
-   wait.until(lambda d: context.browser.find_element(By.ID, 'APjFqb')).is_displayed()
-   context.browser.find_element(By.ID, 'APjFqb').send_keys('twitter')
-   context.browser.find_element(By.ID, 'APjFqb').send_keys(Keys.RETURN)
+@given("I am on the google home page")
+def step_impl(context):
+    context.execute_steps(u'''
+   when we visit google
+   then it does have a title "Google"
+   ''')
+    wait = WebDriverWait(context.browser, timeout=10)
+    wait.until(lambda d: context.browser.find_element(By.ID, 'L2AGLb')).is_displayed()
+    context.browser.find_element(By.ID, 'L2AGLb').click()
 
-   wait.until(lambda d: context.browser.find_element(By.XPATH, '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a')).is_enabled()
-   context.browser.find_element(By.XPATH, '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a').click()
 
-   assert context.browser.title == "X. It’s what’s happening / X"
+@when("I search for twitter")
+def step_impl(context):
+    wait = WebDriverWait(context.browser, timeout=10)
+    wait.until(lambda d: context.browser.find_element(By.ID, 'APjFqb')).is_displayed()
+    context.browser.find_element(By.ID, 'APjFqb').send_keys('twitter')
+    context.browser.find_element(By.ID, 'APjFqb').send_keys(Keys.RETURN)
+
+
+@then("I see twitter in the search results")
+def step_impl(context):
+    wait = WebDriverWait(context.browser, timeout=10)
+    wait.until(lambda d: context.browser.find_element(By.XPATH,
+                                                      '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a')).is_enabled()
+    result_name = context.browser.find_element(By.XPATH,
+                                               '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/h3').text
+    assert result_name == "X. It's what's happening / X"
+
+
+@given("I have searched for Twitter on google")
+def step_impl(context):
+    context.execute_steps(u'''
+       given I am on the google home page
+       when I search for twitter
+       then I see twitter in the search results
+       ''')
+
+
+@when("I click on Twitter in the search results")
+def step_impl(context):
+    context.browser.find_element(By.XPATH,
+                                 '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a').click()
+
+@then("I am on the Twitter home page")
+def step_impl(context):
+    wait = WebDriverWait(context.browser, timeout=10)
+    wait.until(lambda d: context.browser.title is not None)
+    title= context.browser.title
+    assert title == "X. It’s what’s happening / X", "actual: " + title
